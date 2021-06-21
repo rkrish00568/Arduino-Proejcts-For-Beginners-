97e1bb40e089b5f802ac715ec22ca084f3c31f42
Nimisha Chitriv <80246121+NimishaChitriv@users.noreply.github.com>
2021-06-21 17:43:20 +0530

    Add files via upload

---
+++ night_lamp.ino
@@ -0,0 +1,33 @@
+#define led 4 
+#define sensor 2
+int irs ;
+int count=1;
+
+void setup()
+{
+  pinMode (irs,INPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+  irs= digitalRead(sensor);
+  
+  if ((irs==0)&&(count==1))
+  {
+    digitalWrite(led,HIGH);
+    count=2;
+    irs=1;
+    delay(300);
+   
+  }
+  if ((irs==0)&&(count==2))
+  {
+    digitalWrite(led,LOW);
+    count=1;
+    irs=1;
+    delay(300);
+  }
+  
+}
+
